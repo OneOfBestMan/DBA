f5cd301c53e56838493104c89d8b22f374e6caff
rajjeet <rajjeet.phull@gmail.com>
2017-06-14 21:53:37 -0400

    Added buffer pool info and other small adjustments

--- database_compression.sql
+++ database_compression.sql
@@ -1,8 +1,8 @@
-USE AdventureWorks2008
+USE AW2012
 GO
 
-ALTER PROCEDURE dbo.GetCompressionData 
-AS 
+--ALTER PROCEDURE dbo.GetCompressionData 
+--AS 
 
 DECLARE @tablename NVARCHAR(300)
 DECLARE @tableschema NVARCHAR(300)
@@ -57,17 +57,76 @@ begin
 	FETCH NEXT FROM table_cursor INTO @tablename, @tableschema
 END
 
-SELECT CD.*, [IndexName] = i.name FROM (
-	SELECT Type = 'ROW',* 
+SELECT 
+	CD.*
+	,i.name									AS [IndexName]
+	,count(distinct b.page_id)				AS [Buffer Count]
+	,count(distinct b.page_id) * 128		AS [Buffer Count (MB)]
+	
+FROM (
+	SELECT 
+		'ROW'																												AS [Type]
+		,[object_name]																										AS [Object]
+		,[schema_name]																										AS [Schema]
+		,[index_id]																											AS [Index ID]
+		,[partition_number]																									AS [Partition]
+		,[size_with_current_compression_setting(KB)]/1024																	AS [Size (MB)]
+		,[size_with_requested_compression_setting(KB)]/1024																	AS [Size with Compression (MB)]
+		,([size_with_current_compression_setting(KB)]- [size_with_requested_compression_setting(KB)])/1024					AS [Size Savings (MB)]
+		,case 
+			when [size_with_current_compression_setting(KB)] = 0 THEN 0 
+			ELSE cast(100. * (([size_with_current_compression_setting(KB)] - [size_with_requested_compression_setting(KB)])
+			/([size_with_current_compression_setting(KB)] * 1.0)) as decimal(10,1)) END										AS [Size Savings (Pct)]		
+		,[sample_size_with_current_compression_setting(KB)]/1024															AS [Sample Size (MB)]
+		,[sample_size_with_requested_compression_setting(KB)] /1024															AS [Sample Size with Compression (MB)]
+		,([sample_size_with_current_compression_setting(KB)] - [sample_size_with_requested_compression_setting(KB)])/1024	AS [Sample Size Savings (MB)]
 	FROM @rowCompressionData 
 	UNION all
-	SELECT Type = 'PAGE',* 
+	SELECT 
+		'PAGE'																												AS [Type]
+		,[object_name]																										AS [Object]
+		,[schema_name]																										AS [Schema]
+		,[index_id]																											AS [Index ID]
+		,[partition_number]																									AS [Partition]
+		,[size_with_current_compression_setting(KB)]/1024																	AS [Size (MB)]
+		,[size_with_requested_compression_setting(KB)]/1024																	AS [Size with Compression (MB)]
+		,([size_with_current_compression_setting(KB)]- [size_with_requested_compression_setting(KB)])/1024					AS [Size Savings (MB)]
+		,case 
+			when [size_with_current_compression_setting(KB)] = 0 THEN 0 
+			ELSE cast(100. * (([size_with_current_compression_setting(KB)] - [size_with_requested_compression_setting(KB)])
+			/([size_with_current_compression_setting(KB)] * 1.0)) as decimal(10,1)) END										AS [Size Savings (Pct)]		
+		,[sample_size_with_current_compression_setting(KB)]/1024															AS [Sample Size (MB)]
+		,[sample_size_with_requested_compression_setting(KB)] /1024															AS [Sample Size with Compression (MB)]
+		,([sample_size_with_current_compression_setting(KB)] - [sample_size_with_requested_compression_setting(KB)])/1024	AS [Sample Size Savings (MB)]
 	FROM @pageCompressionData 
 ) CD
-	left join sys.indexes i on CD.index_id = i.index_id and OBJECT_ID('[' + CD.[schema_name] + '].[' + CD.OBJECT_NAME + ']', 'U') = i.OBJECT_ID 
-ORDER BY ([size_with_requested_compression_setting(KB)] - [size_with_current_compression_setting(KB)]) 
+	left join sys.indexes i with (nolock)
+		on CD.[Index ID] = i.index_id and OBJECT_ID('[' + CD.[Schema] + '].[' + CD.[Object] + ']', 'U') = i.OBJECT_ID 
+	left join sys.partitions p with (nolock)
+		on p.index_id = i.index_id and p.object_id = i.object_id
+	left join sys.allocation_units a with (nolock)
+		on p.hobt_id = a.container_id
+	left join sys.dm_os_buffer_descriptors b with (nolock)
+		on a.allocation_unit_id = b.allocation_unit_id
+group by 
+	i.name	
+	,CD.[Type]
+	,CD.[Object]
+	,CD.[Schema]
+	,CD.[Index ID]
+	,CD.[Partition]
+	,CD.[Size (MB)]
+	,CD.[Size with Compression (MB)]
+	,CD.[Size Savings (MB)]
+	,CD.[Size Savings (Pct)]		
+	,CD.[Sample Size (MB)]
+	,CD.[Sample Size with Compression (MB)]
+	,CD.[Sample Size Savings (MB)]
+ORDER BY [Size Savings (MB)] desc
 
 CLOSE table_cursor
 DEALLOCATE table_cursor
+
+
 
 
